85792963d7f2411619e894f7edca4dc971591aff
buptchi <buptcqz@qq.com>
2019-08-09 23:27:57 +0800

    use class

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class A {
+public:
+	A(int b):a(b) {}
+	int a;
+};
+
 int main() {
 	std::cout << "hello chi from windows 10" << std::endl;
 	std::cout << "vscode is so good" << std::endl;
+	A ca(5);
+	std::cout << "class A member a=" << ca.a << std::endl;
 	return 0;
 }
